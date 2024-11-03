f6e327006c4b66d92f6ebdc60d8ef90136805543
RamAvni <ramavni@hilma.tech>
2024-11-03 11:39:31 +0200

    Hi, I will bring you chocolate tomorrow

---
+++ Ugat-Banana/src/components/Login.jsx
@@ -0,0 +1,8 @@
+export default function Login(props) {
+    return (
+        <>
+            <label htmlFor="name">Name</label>
+            <input type="text" name="name" />
+        </>
+    )
+}
